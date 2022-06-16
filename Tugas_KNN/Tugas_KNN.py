26a54e7a7e70ded0f1502c0178402ce01c701267
Rizky Fajri <mansamussolini@gmail.com>
2022-06-17 00:23:36 +0700

    Add project files.

---
+++ Tugas_KNN/Tugas_KNN.py
@@ -0,0 +1,10 @@
+from typing import Dict, Match
+from collections import defaultdict
+import random 
+import pandas as pd
+from pandas.core.frame import DataFrame
+import xlsxwriter as xw
+import itertools as itl
+import string
+
+
